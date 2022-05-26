7dfa9be6e31a0b9b464491bca3d3cd4ba36b8584
seairs <sea.airs77@gmail.com>
2022-05-25 23:46:19 -0400

    added schema, validations

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                   :integer          not null, primary key
+#  name                 :string
+#  age                  :string
+#  cuteness             :integer
+#  softness             :integer
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
+#  booking_id           :integer
+#
 class Kitten < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :age, presence: true
+  validates :cuteness, presence: true, length: { maximum: 20 }
+  validates :softness, presence: true, length: { maximum: 20 }
 end
